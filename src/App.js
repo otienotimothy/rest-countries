2a4752ed7c8934e8964d6de86435ae8b6202d9cd
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-04-28 01:44:10 +0300

    Add styling for the search component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Navbar } from './components/navbar/Navbar';
+import {Search} from './components/search/Search'
 
 function App() {
 
@@ -9,6 +10,10 @@ function App() {
 		<QueryClientProvider client={queryClient}>
 			<div className="min-h-screen bg-slate-700">
 				<Navbar />
+				<div className="container mx-auto pt-8">
+					<Search />
+				</div>
+				
 			</div>	
 		</QueryClientProvider>
 	);
--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,16 +1,22 @@
-
-function Search({searchTerm, onChange, onSubmit }){
-    return (
-			<form onSubmit={onSubmit}>
-				<label htmlFor="search">
-					<i class="fa-solid fa-magnifying-glass"></i>
+export function Search({ searchTerm, onChange, onSubmit }) {
+	return (
+		<form
+			onSubmit={onSubmit}
+			className="rounded-lg bg-slate-800 w-4/5 mx-auto md:w-1/3 md:mx-0"
+		>
+			<div className="flex items-center">
+				<label htmlFor="search" className="mx-4">
+					<i className="fa-solid fa-magnifying-glass text-2xl text-gray-50"></i>
 				</label>
 				<input
 					type="search"
 					name="search"
+					className="w-4/5 h-12 bg-slate-800 border-none outline-none focus:ring-0 text-gray-50"
+					placeholder="Search for a country..."
 					value={searchTerm}
 					onChange={onChange}
 				/>
-			</form>
-		);
+			</div>
+		</form>
+	);
 }
