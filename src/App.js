cac6b931298c237855c2c4b1a0ca4e24e43ef8d9
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-04-28 16:08:21 +0300

    Add functionality to manage the state of the search component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
+import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Navbar } from './components/navbar/Navbar';
 import {Search} from './components/search/Search'
 
 function App() {
 
-  let queryClient = new QueryClient()
+	const [searchTerm, setSearchTerm] = useState('');
+
+	const onChange = (e) => setSearchTerm(e.target.value)
+
+	const onSubmit = (e) => {
+		e.preventDefault()
+		console.log(searchTerm)
+	}
+
+	let queryClient = new QueryClient()
 
 	return (
 		<QueryClientProvider client={queryClient}>
 			<div className="min-h-screen bg-slate-700">
 				<Navbar />
 				<div className="container mx-auto pt-8">
-					<Search />
+					<div>
+						<Search searchTerm={searchTerm} onChange={onChange} onSubmit={onSubmit} />
+					</div>
+					<div>
+					</div>
 				</div>
 				
 			</div>	
