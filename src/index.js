c8a5da91525ede2cf900fa643cc5acf80165ee67
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-04-29 13:19:35 +0300

    Move react-query initialisation to the index file

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// React Query Init
+import { QueryClient, QueryClientProvider } from "react-query";
+
+let queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
