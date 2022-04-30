623cd164669ac47e4f21c76d425299336bcd2abf
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-04-30 10:34:02 +0300

    Add page routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Components Import
 import { Navbar } from "./components";
-import { Home } from './routes/Home';
+import { Home } from "./routes/Home";
 
 function App() {
-
 	return (
 		<div className="min-h-screen bg-slate-700">
 			<Navbar />
 			<div className="container mx-auto pt-8">
-				<Home />
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<Home />} />
+					</Routes>
+				</BrowserRouter>
 			</div>
 		</div>
 	);
